e9c186e5eaded553aabed8232d1ab1f9fc9358c6
LinusW-HWR <s_widing21@stud.hwr-berlin.de>
2024-05-20 11:09:00 +0000

    Update api

--- api.py
+++ api.py
@@ -1,35 +1,77 @@
 from flask import Blueprint, jsonify, request
 from flask_socketio import emit
+from navigator import navigate_to_room
+
 
 api_blueprint = Blueprint('api', __name__)
 
 # Sample list of requests
-requests = []
+reqs = [{
+            "id": 2,
+            "name": "Helga",
+            "room": 2,
+            "type": "delivery",
+            "medicine": "painkiller",
+            "status": "done"
+        },
+        {
+            "id": 1,
+            "name": "Berta",
+            "room": 1,
+            "type": "collection",
+            "status": "pending"
+        }]
 
 @api_blueprint.route('/', methods=['GET'])
 def get_requests():
-    return jsonify(requests)
+    return jsonify(reqs)
 
-@api_blueprint.route('/request', methods=['PUT'])
+@api_blueprint.route('/request', methods=['PUT', "GET"])
 def add_request():
-    data = request.json
-    new_request = {
-        "id": len(requests) + 1,
-        "name": data['name'],
-        "room": data['room'],
-        "type": data['type'],
-        "status": "in progress"
-    }
-    requests.insert(0, new_request)
+    if request.method == "PUT":
+        data = request.json
+        new_request = {
+            "id": len(reqs) + 1,
+            "name": data['name'],
+            "room": data['room'],
+            "type": data['type'],
+            "status": "pending"
+        }
+        reqs.insert(0, new_request)
+        
+
+        emit('new_request', new_request, namespace="/", broadcast=True)
+        print("success")
+        print(reqs)
+        return jsonify({"message": "Request added successfully"}), 201
+
+    if request.method == "GET":
+        # Find the request with the specified ID
+        req_id = int(request.args.get("id"))
+        for r in reqs:
+            if r['id'] == req_id:
+                return jsonify(r)
+        return jsonify({"error": "Request not found"}), 404
     
 
-    emit('new_request', new_request, namespace="/", broadcast=True)
-    print("success")
-    print(requests)
-    return jsonify({"message": "Request added successfully"}), 201
+@api_blueprint.route('/update_req', methods=['POST'])
+def update_req():
+    req_id = int(request.args.get("id"))
+    for r in reqs:
+        if r["id"] == req_id:
+            for key, value in request.args.items():
+                if key == "id":
+                    continue
+                r[key] = value
+                emit('new_request', r, namespace="/", broadcast=True)
+                return jsonify({"message": "Req updated successfully!"}), 201
+
+        
 
 @api_blueprint.route('/send_robot', methods=['PUT'])
 def send_robot():
-    req_id = request.args.get("id")
-    print(f"Sending robot: {req_id}")
-    return jsonify({"message": "Robot send successfully"}), 200
+    room_id = int(request.args.get("room_id"))
+    
+    navigate_to_room(room_id)
+    
+    return jsonify({"message": "Robot has arrived"}), 200
